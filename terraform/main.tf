42458595f1d2998a65cca4424706c71b9abc3adf
anoopjoseph02 <36333856+anoopjoseph02@users.noreply.github.com>
2026-03-25 23:52:57 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,2 @@
-provider "azurerm" {
-  features {}
-}
 
-resource "azurerm_resource_group" "main" {
-  name     = var.resource_group
-  location = var.location
-}
-
-resource "azurerm_storage_account" "main" {
-  name                     = var.storage.name
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = var.storage.account_replication_type
-
-  depends_on = [
-    azurerm_resource_group.main
-  ]
-}
 
