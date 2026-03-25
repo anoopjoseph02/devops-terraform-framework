e025c7e1695e22332dfeaa84f0757f47c46cf935
anoopjoseph02 <36333856+anoopjoseph02@users.noreply.github.com>
2026-03-25 23:53:30 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,2 @@
-variable "resource_group" {
-  description = "The name of the resource group"
-  type        = string
-}
 
-variable "location" {
-  description = "The Azure location for all resources"
-  type        = string
-}
-
-variable "storage" {
-  description = "Configuration for the storage account"
-  type = object({
-    name                     = string
-    account_replication_type = string
-  })
-}
 
